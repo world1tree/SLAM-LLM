3e576aa1566e4e82eeadd94c5579e78eeb4a2002
xx <xx@gmail.com>
2024-10-21 20:39:19 +0800

    update scripts

--- examples/sttr/scripts/decode.sh
+++ examples/sttr/scripts/decode.sh
@@ -8,11 +8,14 @@ run_dir=/public/home/zhxgong/hxdou/STTR/SLAM-LLM
 cd $run_dir
 code_dir=examples/sttr
 
-speech_encoder_path=/public/home/zhxgong/.cache/whisper/tiny.en.pt
+speech_encoder_path=/public/home/zhxgong/.cache/whisper/large-v3.pt
 llm_path=/public/home/zhxgong/hxdou/STTR/pretrained/gemma2b
 
-output_dir=/public/home/zhxgong/hxdou/STTR/SLAM-LLM/examples/sttr/output/whisper-linear-llama3-20241010
-ckpt_path=$output_dir/asr_epoch_1_step_95000
+# output_dir=/public/home/zhxgong/hxdou/STTR/SLAM-LLM/examples/sttr/output/whisper-linear-llama3-20241012/asr_epoch_3_step_111842
+# output_dir=/public/home/zhxgong/hxdou/STTR/SLAM-LLM/examples/sttr/output/whisper-linear-llama3-20241012/asr_epoch_2_step_95921
+# output_dir=/public/home/zhxgong/hxdou/STTR/SLAM-LLM/examples/sttr/output/whisper-linear-llama3-20241015/asr_epoch_2_step_45921
+output_dir=/public/home/zhxgong/hxdou/STTR/SLAM-LLM/examples/sttr/output/whisper-linear-llama3-20241020/asr_epoch_2_step_45921
+ckpt_path=$output_dir
 split=test
 val_data_path=/public/home/zhxgong/hxdou/STTR/data/sttr-${split}.jsonl
 decode_log=$ckpt_path/decode_${split}_beam3
@@ -29,12 +32,12 @@ python $code_dir/inference_sttr_batch.py \
         ++model_config.encoder_name=whisper \
         ++model_config.encoder_projector_ds_rate=5 \
         ++model_config.encoder_path=$speech_encoder_path \
-        ++model_config.encoder_dim=384 \
+        ++model_config.encoder_dim=1280 \
         ++model_config.encoder_projector=linear \
         ++dataset_config.dataset=speech_dataset \
         ++dataset_config.val_data_path=$val_data_path \
         ++dataset_config.input_type=mel \
-        ++dataset_config.mel_size=80 \
+        ++dataset_config.mel_size=128 \
         ++dataset_config.inference_mode=true \
         ++dataset_config.audio_root=$audio_root \
         ++train_config.model_name=asr \
@@ -42,14 +45,14 @@ python $code_dir/inference_sttr_batch.py \
         ++train_config.freeze_llm=true \
         ++train_config.batching_strategy=custom \
         ++train_config.num_epochs=1 \
-        ++train_config.val_batch_size=1 \
+        ++train_config.val_batch_size=4 \
         ++train_config.num_workers_dataloader=2 \
         ++train_config.output_dir=$output_dir \
         ++decode_log=$decode_log \
         ++ckpt_path=$ckpt_path/model.pt \
-        ++log_config.log_file=$output_dir/log.txt
+        ++log_config.log_file=$output_dir/log.txt \
+        ++train_config.use_peft=true
         # ++peft_ckpt=$ckpt_path \
-        # ++train_config.use_peft=true \
         # ++train_config.peft_config.r=32 \
         # ++dataset_config.normalize=true \
         # ++model_config.encoder_projector=q-former \
