c2035656dbe7546e0bcc458799695b1aacfd6018
xx <xx@gmail.com>
2024-10-21 20:38:25 +0800

    change lora default configs

--- examples/sttr/asr_config.py
+++ examples/sttr/asr_config.py
@@ -25,8 +25,8 @@ class ModelConfig:
 class PeftConfig:
     peft_method: str = "lora" # None , llama_adapter, prefix
     r: int = 8
-    lora_alpha: int = 32
-    target_modules: List = field(default_factory=lambda: [ "q_proj", "v_proj" ])
+    lora_alpha: int = 16
+    target_modules: List = field(default_factory=lambda: [ "q_proj", "v_proj", "k_proj", "o_proj", "gate_proj", "up_proj", "down_proj" ])
     bias: str = "none"
     task_type: str = "CAUSAL_LM"
     lora_dropout: float = 0.05
